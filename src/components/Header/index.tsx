2e8ad6fa1bf905e1ff57f6422e0d162824d83cf0
Pablo Henrique de Souza <pablo.henrique@cubos.io>
2022-12-05 16:01:39 +0000

    feat(Header): sticky

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import { AccountCircle, Logout as LogoutIcon } from "@mui/icons-material";
 import {
   AppBar,
-  Box,
   Button,
   IconButton,
   ListItemIcon,
@@ -33,60 +32,58 @@ export function Header({}: Props) {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component={Link}
-            to="/terminals"
-            sx={{ color: "inherit", textDecoration: "none", flexGrow: 1 }}
-          >
-            ON GO Cargas
-          </Typography>
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component={Link}
+          to="/terminals"
+          sx={{ color: "inherit", textDecoration: "none", flexGrow: 1 }}
+        >
+          ON GO Cargas
+        </Typography>
 
-          <Button component={Link} to="/terminals">
-            Terminais
-          </Button>
+        <Button component={Link} to="/terminals">
+          Terminais
+        </Button>
 
-          {Boolean(user) && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle alphabetic="On Go Cargas" />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleLogout}>
-                  <ListItemIcon>
-                    <LogoutIcon fontSize="small" />
-                  </ListItemIcon>
-                  Logout
-                </MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
+        {Boolean(user) && (
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle alphabetic="On Go Cargas" />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleLogout}>
+                <ListItemIcon>
+                  <LogoutIcon fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 }
