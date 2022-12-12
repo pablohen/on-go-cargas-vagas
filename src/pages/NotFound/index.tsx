af3a1cb4c6b5e5e8b32a2fd692a22b5d804fb7ec
Pablo Henrique de Souza <pablo.henrique@cubos.io>
2022-12-12 17:21:13 +0000

    feat: NotFound page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,10 @@
+import { Container, Typography } from "@mui/material";
+
+export default function NotFound() {
+  return (
+    <Container>
+      <Typography variant="h4">404</Typography>
+      <Typography variant="h6">Not found</Typography>
+    </Container>
+  );
+}
