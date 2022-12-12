e47b610f16951fa3a3997e1b472728d44a476d40
Pablo Henrique de Souza <pablo.henrique@cubos.io>
2022-12-12 17:21:01 +0000

    feat: Fallback

---
+++ src/components/Fallback/index.tsx
@@ -0,0 +1,16 @@
+import { Box, CircularProgress } from "@mui/material";
+
+export function Fallback() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
